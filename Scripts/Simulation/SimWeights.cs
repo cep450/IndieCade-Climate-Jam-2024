f1c76c2772c5ee264894c70d030b8dbd08f86155
Salmon828 <156275682+Salmon828@users.noreply.github.com>
2024-06-27 12:56:29 -0700

    Added GUnit4 to test new functions. Not needed for final game, just programming testing.

--- Scripts/Simulation/SimWeights.cs
+++ Scripts/Simulation/SimWeights.cs
@@ -3,18 +3,18 @@ using System;
 
 public class SimWeights {
 
-    
+	
 
-    // associate each index with a name for what it represents 
-    public enum Weight {
-        TIME = 0,
-        SAFETY = 1,
-        EMISSIONS = 2
-    }
+	// associate each index with a name for what it represents 
+	public enum Weight {
+		TIME = 0,
+		SAFETY = 1,
+		EMISSIONS = 2
+	}
 
-    // example use: 
-    // weights[Weight.SAFETY] = 0.5f;
+	// example use: 
+	// weights[Weight.SAFETY] = 0.5f;
 
-    public float[] weights;
+	public float[] weights;
 
 }
