a3c2d411734f619b9c5276ea336f934f0261301a
avelichkov <108222811+avelichkov@users.noreply.github.com>
2024-06-28 16:29:24 -0700

    actual push fr fr

--- Scripts/Simulation/SimGrid.cs
+++ Scripts/Simulation/SimGrid.cs
@@ -25,8 +25,7 @@ public partial class SimGrid : Node
 		{
 			grid[x] = new SimTile[height];
 			for (int y = 0; y < height; y++)
-			{
-				Vector2 position = new Vector2(x, y);
+			{				Vector2 position = new Vector2(x, y);
 				grid[x][y] = new SimTile(position, 1); // Initialize each tile with a position and capacity
 				AddChild(grid[x][y]); // Add each tile as a child node (optional)
 			}
