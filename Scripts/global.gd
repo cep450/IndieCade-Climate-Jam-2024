2bda661ff7491ec11d02339c5ac0ad20e9e470b8
avelichkov <108222811+avelichkov@users.noreply.github.com>
2024-07-18 00:01:56 -0700

    some UI fixes, you can now build roads (exclusively) highlight applies to full tile now and works, also there should be no crashes fixed input handling bug

--- Scripts/global.gd
+++ Scripts/global.gd
@@ -2,6 +2,7 @@ extends Node
 
 var inDevMode: bool = true
 @onready var sim: Node = $"../Main/Simulation"
+var count: int = 0
 
 enum InfraType {
 	HOME,	
@@ -17,5 +18,3 @@ enum InfraType {
 	PARKINGLOT,
 	BIKERACK,
 }
-
-@export var resource: Array[SimInfraType]
