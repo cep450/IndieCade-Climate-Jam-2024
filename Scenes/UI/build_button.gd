d382cd1db31636e77455f36377e4e347f1d09b04
hlee1400 <leehs001024@gmail.com>
2024-07-18 22:29:06 -0500

    Removed the place holder music and added the final version; Imported title menu(Main_Menu) assets; Deleted the existing tutorial menu, instead will be implementing in game tutorial; Implemented the UI assets for the title menu The buttons are not clickable anymore, needs to be fixed The buttons are not resizing with the screen, needs to be fixed

--- Scenes/UI/build_button.gd
+++ Scenes/UI/build_button.gd
@@ -1,19 +1,20 @@
-extends Button
 
-var sim
+#extends Button
+
+#var sim
 
 # Change to the enum later
-func initialize(param_type):
-	sim = get_parent().get_parent().sim
-	match param_type:
-		Global.InfraType.ROAD:
-			text = "road"
-		Global.InfraType.HOUSE:
-			text = "house"
-		Global.InfraType.BUILDING:
-			text = "building"
+#func initialize(param_type):
+	#sim = get_parent().get_parent().sim
+	#match param_type:
+		#Global.InfraType.ROAD:
+			#text = "road"
+		#Global.InfraType.HOUSE:
+			#text = "house"
+		#Global.InfraType.BUILDING:
+			#text = "building"
 
-func _on_pressed():
+#func _on_pressed():
 	#add infra of the appropriate type
-	pass
+	#pass
 	
---
+++ Scripts/Simulation/Main_Menu.gd
@@ -0,0 +1,58 @@
+
+
+extends Control
+
+
+#@onready var start_button = $MarginContainer/HBoxContainer/VBoxContainer/Start_Button as Button
+#@onready var tutorial_button = $MarginContainer/HBoxContainer/VBoxContainer/Tutorial_Button as Button
+#@onready var start_level = preload("res://Scenes/main.tscn") as PackedScene
+#@onready var exit_button = $MarginContainer/HBoxContainer/VBoxContainer/Exit_Button as Button
+
+
+# Called when the node enters the scene tree for the first time.
+#func _ready():
+	#start_button.button_down.connect(on_start_pressed)
+	#tutorial_button.button_down.connect(on_tutorial_pressed)
+	#exit_button.button_down.connect(on_exit_pressed)
+	
+
+#func on_start_pressed() -> void:
+	#get_tree().change_scene_to_packed(start_level)
+
+#func on_tutorial_pressed() -> void:
+	#pass
+	
+#func on_exit_pressed() -> void:
+	#get_tree().quit()
+	
+	
+
+
+@onready var start_button = $VBoxContainer/Start_Game_Button/StartButton
+@onready var quit_button = $VBoxContainer/Quit_Game_Button/QuitButton
+@onready var start_level = preload("res://Scenes/main.tscn") as PackedScene
+
+func _ready():
+	start_button.button_down.connect(on_start_pressed)
+#	tutorial_button.button_down.connect(on_tutorial_pressed)
+#	exit_button.button_down.connect(on_exit_pressed)
+	
+
+func on_start_pressed() -> void:
+	get_tree().change_scene_to_packed(start_level)
+
+
+#func _ready():
+	# Ensure the start button is receiving input events
+	#start_button.connect("gui_input", Callable(self, "_on_start_button_input"))
+
+#func _on_start_button_input(event):
+	#if event is InputEventMouseButton and event.pressed:
+		#if event.button_index == MOUSE_BUTTON_LEFT:
+			#if start_button.get_rect().has_point(start_button.get_local_mouse_position()):
+#				print("Input Event Detected on Start Button")
+#				_on_start_button_pressed()
+
+#func _on_start_button_pressed():
+#	print("Start button pressed!")
+#	get_tree().change_scene_to_packed(start_level)
