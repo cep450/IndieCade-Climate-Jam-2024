3716e1ca99ed80240e1ff1bffcb6a098d22f8a8d
avelichkov <108222811+avelichkov@users.noreply.github.com>
2024-07-18 19:20:50 -0700

    set up way to call tick on UI and View stuff

--- Scripts/UI/ui.gd
+++ Scripts/UI/ui.gd
@@ -7,13 +7,15 @@ extends Control
 @export var emissions_meter: TextureProgressBar
 @export var build_buttons: HBoxContainer
 
-@export var speed_controller: PanelContainer
+@onready var speed_controller: PanelContainer = $SpeedController
 @export var speed_controller_images: Array[Texture2D]
 
+@onready var happiness_meter: VBoxContainer = $"HappinessMeter"
 @export var happiness_meter_images: Array[Texture2D]
 
+@onready var world: Node3D = $"../View/World"
 #Public Variables
-var happiness: HappinessState
+var happiness_state: HappinessState
 var speed: SpeedState
 
 # Private Variables
@@ -32,11 +34,15 @@ enum HappinessState
 	GREAT, NEUTRAL, BAD
 }
 
+func tick():
+	pass
+
 func _ready():
 	speed = SpeedState.PLAY
+	build_buttons.get_parent().visible = false
 	speed_controller.get_node("Image").texture = speed_controller_images[speed]
 	emissions_meter.max_value = sim_emissions_meter.GetEmissionsCap()
-	$"../View/World".tile_clicked.connect(on_tile_clicked)
+	world.tile_clicked.connect(on_tile_clicked)
 
 func _process(_delta):
 	# Update Emissions Meter
@@ -44,17 +50,34 @@ func _process(_delta):
 	var percent = int(100 * emissions_meter.value/emissions_meter.max_value)
 	emissions_meter.get_node("Percentage").text = str(percent) + "%"
 	# Update Happines State
-	
+	# TODO request this info from simulation.
+	var happiness = sim.SupportPool.GlobalHappiness
+	happiness_meter.get_node("Text").text = str(happiness)
+	if happiness > 66:
+		happiness_state =  HappinessState.GREAT
+	elif happiness > 33:
+		happiness_state = HappinessState.NEUTRAL
+	else:
+		happiness_state = HappinessState.BAD
+	happiness_meter.get_node("Image").texture = happiness_meter_images[happiness_state]
+	# Update Support
+	var support = sim.SupportPool.Support
+	$SupportTemp.text = "Support: " + str(support)
 	
 func on_tile_clicked(x: int, y: int) -> void:
-	# Follow in the footsteps of Anikin and delete all the children
-	for child in build_buttons.get_children():
-		child.queue_free()
-	# Temp func to add test ones
-	var types = [Global.InfraType.WORK]
-	for type in types:
-		var instance = button.instantiate()
-		build_buttons.add_child(instance)
-		instance.initialize(load("res://Resources/InfraTypes/road.tres"), x, y)
-	return
+	# Only display if tile selected checked by if it's a valid index.
+	if Global.current_tile == Vector2(-1,-1):
+		build_buttons.get_parent().visible = false
+		return
+	else:
+		build_buttons.get_parent().visible = true
+		# Follow in the footsteps of Anikin and delete all the children
+		for child in build_buttons.get_children():
+			child.queue_free()
+		# Temp func to add test ones
+		for type in 1:
+			var instance = button.instantiate()
+			build_buttons.add_child(instance)
+			instance.initialize(load("res://Resources/InfraTypes/road.tres"), x, y)
+		return
 	
