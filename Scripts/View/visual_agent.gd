c9ce52ca57e899f2cb4cedf02d14dd7fb7dab133
pc <61981562+cep450@users.noreply.github.com>
2024-07-31 18:00:26 -0400

    agents choose color variants, pedestrians animate, agents turn towards where they're moving

--- Scripts/View/visual_agent.gd
+++ Scripts/View/visual_agent.gd
@@ -1,7 +1,6 @@
 extends Node3D
 
 # approach 1: each agent has a model of each type that they can turn on and off
-var models
 
 # approach 2: using an object pooler
 # TODO if approach 1 is still too much of a performance impact 
@@ -22,14 +21,25 @@ var simAgent
 
 var speedMultiplier = 0.02
 
+# TODO we'll want to export these probably 
+static var modelsPedestrian = ["res://Models/Agent_Person/Agent_Animated/agent_person_1.tscn", "res://Models/Agent_Person/Agent_Animated/agent_person_2.tscn", "res://Models/Agent_Person/Agent_Animated/agent_person_3.tscn", "res://Models/Agent_Person/Agent_Animated/agent_person_4.tscn"]
+static var modelsCyclist = ["res://Models/Agent_Biker/agent_biker_1.tscn"]
+static var modelsCars = ["res://Models/Car/Car_Yellow_Driving.tscn", "res://Models/Car/Car_Blue_Driving.tscn", "res://Models/Car/Car_Green_Driving.tscn", "res://Models/Car/car_red_driving.tscn"]
+var models = []
+
 func _init():
 	visible = false
 
 	#TODO make it so arbitrary modes are possible and it uses paths from SimVehicleTypes instead of hardcoding & can pick randomly from a list with probability weights
+	
+	var randP = randi_range(0, modelsPedestrian.size() - 1)
+	var randCy = randi_range(0, modelsCyclist.size() - 1)
+	var randCa = randi_range(0, modelsCars.size() - 1)
+	
 	models = [
-		load("res://Models/Agent_Person/Agent_Person_1.gltf").instantiate(),
-		load("res://Models/Car/Car_Yellow_Driving.tscn").instantiate(),
-		load("res://Models/Agent_Biker/Agent_Biker_1.gltf").instantiate()
+		load(modelsPedestrian[randP]).instantiate(),
+		load(modelsCars[randCa]).instantiate(),
+		load(modelsCyclist[randCy]).instantiate()
 	]
 	for m in models:
 		add_child(m)
@@ -48,6 +58,8 @@ func _process(delta):
 			moving = false
 			simAgent.MoveNext()
 			return
+		if(position != posTo):
+			look_at(posTo)
 	else:
 		speed = max(0, speed - (speedDecayRate * delta))	
 
@@ -64,9 +76,6 @@ func Move(posStart: Vector3, posEnd: Vector3, _maxSpeed: float, _acceleration: f
 	speedDecayRate = _decceleration
 	moveVector = (posEnd - posStart).normalized()
 	distance = (posEnd - posStart).length()
-
-	#rotate model to direction facing 
-	print(moveVector)
 
 	percentTravelled = 0
 	moving = true 
